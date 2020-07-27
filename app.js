b4626f2dd0b9c45868b43d5bdf7422c034482c56
luke-h1 <luke.howsam@yahoo.com>
2020-07-27 12:56:41 +0100

    getting response from API

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-
-
-
-    
-const myform = document.querySelector('.test'); 
-myform.addEventListener('submit', function(e){
-    e.preventDefault(); 
-    const number = document.getElementById('number'); 
-    const API_URL = 'http://numbersapi.com/' + number; 
-    let response =  fetch(API_URL);   
-    let result =  response.text();  
-    result.appendChild()
-    
-}); 
-
-
+const myform = document.querySelector('.test');
+myform.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const number = document.getElementById('number').value;
+    const API_URL = 'http://numbersapi.com/' + number;
+    let response = fetch(API_URL);   
+    console.log(response)
+});

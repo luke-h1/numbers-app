6008f5a296dde04e4822329c3243965a2ac943c6
luke-h1 <luke.howsam@yahoo.com>
2020-07-27 12:50:10 +0100

    update

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
-// const myForm = document.getElementById('testing');
-// myForm.addEventListener('submit', function (e) {
-//     e.preventDefault();
-//     const userInput = document.getElementById('numberReq').value;
-//     const API_URL = 'http://numbersapi.com/' + userInput;
-
-//     fetch(API_URL)
-//         .then((res) => res.json())
-//         .then((res) => {
-//             console.log(res);
-
-//         });
-// });
-
-const API_URL = 'http://numbersapi.com/random/year';
-const columnElement = document.querySelector('.columns');
-const goButton = document.querySelector('.go-button');
-const loadingElement = document.querySelector('.loading');
 
 
-loadingElement.style.display = 'none';
 
-async function getData() {
-    loadingElement.style.display = '';
-    const response = await fetch(API_URL);
-    const json = await response.json();
-    console.log(json.message);
-    json.message((year) => {
-        columnElement.innerHTML += `
-        <div class="column> 
-        <h1>`;
-    }); 
-    loadingElement.style.display = 'none' 
-}
+    
+const myform = document.querySelector('.test'); 
+myform.addEventListener('submit', function(e){
+    e.preventDefault(); 
+    const number = document.getElementById('number'); 
+    const API_URL = 'http://numbersapi.com/' + number; 
+    let response =  fetch(API_URL);   
+    let result =  response.text();  
+    result.appendChild()
+    
+}); 
 
 
-goButton.addEventListener('click', getData); 

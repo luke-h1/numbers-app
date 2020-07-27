1813faf1a3a3e23bf48cc47d4c0098ab742895cf
luke-h1 <luke.howsam@yahoo.com>
2020-07-27 12:30:46 +0100

    update

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
-const API_URL = 'http://numbersapi.com/random/year?json?' + result;
-// http://numbersapi.com/42/math?callback=showNumber"
-// maybe introduce a math.floor() for random number output... 
+// const myForm = document.getElementById('testing');
+// myForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     const userInput = document.getElementById('numberReq').value;
+//     const API_URL = 'http://numbersapi.com/' + userInput;
 
-const output = document.querySelector('output');
-const btn = document.querySelector('btn');
-const form = document.getElementById('form');
+//     fetch(API_URL)
+//         .then((res) => res.json())
+//         .then((res) => {
+//             console.log(res);
 
-btn.addEventListener('click', getData());
+//         });
+// });
+
+const API_URL = 'http://numbersapi.com/random/year';
+const columnElement = document.querySelector('.columns');
+const goButton = document.querySelector('.go-button');
+const loadingElement = document.querySelector('.loading');
 
 
+loadingElement.style.display = 'none';
 
-function showNumber(str) {
-    document.getElementById('number-fact').innerHTML = str;
-    console.log(str);
+async function getData() {
+    loadingElement.style.display = '';
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    console.log(json.message);
+    json.message((year) => {
+        columnElement.innerHTML += `
+        <div class="column> 
+        <h1>`;
+    }); 
+    loadingElement.style.display = 'none' 
 }
 
-fetch(API_URL)
-    .then((res) => res.json())
-    .then((res) => {
-        console.log(res);
-        const length = res.length;
-        const index = length - 1;
-        document.getElementById('number-fact').innerHTML = str;
-        console.log(str);
-    });
 
-// DOCUMENTATION JSON RESPONSE
-// // text: A string of the fact text itself.
-// found: Boolean of whether there was a fact for the requested number.
-// number: The floating-point number that the fact pertains to. This may be useful for, eg. a /random request or notfound=floor. For a date fact, this is the 1-indexed day of a leap year (eg. 61 would be March 1st).
-// type: String of the category of the returned fact.
-// date (sometimes): A day of year associated with some year facts, as a string.
-// year (sometimes): A year associated with some date facts, as a string.
-// http://numbersapi.com/random/year?json
-// ⇒ {
-//     "text": "2012 is the year that the century's second and last solar transit of Venus occurs on June 6.",
-//     "found": true,
-//     "number": 2012,
-//     "type": "year",
-//     "date": "June 6"
-
-{
-    /* <span id="number-fact"></span> */
-}
-
+goButton.addEventListener('click', getData); 

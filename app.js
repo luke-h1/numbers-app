08699477c72eeabaa759480ce5c1145c6935ea91
luke-h1 <luke.howsam@yahoo.com>
2020-07-27 13:30:56 +0100

    update

--- app.js
+++ app.js
@@ -8,30 +8,15 @@ myform.addEventListener('submit', function (e) {
             console.log(res);
             const found = document.getElementById('found').innerHTML = 'Found: ' + res.found
             const number = document.getElementById('number').innerHTML = 'number ' + res.number
-            const text = document.getElementById('text').innerHTML = res.text
-            const type = document.getElementById('type').innerHTML = 'number ' + res.type
+            const text = document.getElementById('text').innerHTML =  res.text
+            const type = document.getElementById('type').innerHTML = 'number: ' + res.type
             
             
             
-        });
+        }); 
+        found.innerHTML = ''
+        number.innerHTML = ''
+        text.innerHTML = ''
+        type.innerHTML = ''
 });
 
-
-// respose 
-
-
-// "text": "1142 is the year that Henry the Lion becomes Duke of Saxony.",
-// "number": 1142,
-// "found": true,
-// "type": "year"
-
-//  // fix concatenation issue
-//  confirmed.innerHTML = '';
-//  recovered.innerHTML = '';
-//  deaths.innerHTML = '';
-
-
-// <script>
-// document.getElementById("demo").innerHTML =
-// obj.employees[1].firstName + " " + obj.employees[1].lastName;
-// </script>

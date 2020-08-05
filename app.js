c004f3d50226481a1032d21f790b147e9840c7bd
luke-h1 <luke.howsam@yahoo.com>
2020-08-05 18:32:01 +0100

    refactoring some stuff

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const myform = document.querySelector('.test');
-myform.addEventListener('submit', function (e) {
+myform.addEventListener('submit', getResults);
+const API_URL = 'http://numbersapi.com/random/year?json';
+
+function getResults(e) {
     e.preventDefault();
-    const API_URL = 'http://numbersapi.com/random/year?json';
     fetch(API_URL)
         .then((res) => res.json())
         .then((res) => {
-            console.log(res);
-            const found = document.getElementById('found').innerHTML = 'Found: ' + res.found
-            const number = document.getElementById('number').innerHTML = 'number ' + res.number
-            const text = document.getElementById('text').innerHTML =  res.text
-            const type = document.getElementById('type').innerHTML = 'number: ' + res.type
-            
-            
-            
-        }); 
-        found.innerHTML = ''
-        number.innerHTML = ''
-        text.innerHTML = ''
-        type.innerHTML = ''
-});
+            const found = (document.getElementById('found').innerHTML =
+                'Found: ' + res.found);
+            const number = (document.getElementById('number').innerHTML =
+                'number ' + res.number);
+            const text = (document.getElementById('text').innerHTML = res.text);
+            const type = (document.getElementById('type').innerHTML =
+                'number: ' + res.type);
+        });
 
+    found.innerHTML = '';
+    number.innerHTML = '';
+    text.innerHTML = '';
+    type.innerHTML = '';
+}
+
+// e.preventDefault();
+// const API_URL = 'http://numbersapi.com/random/year?json';
+// fetch(API_URL)
+//     .then((res) => res.json())
+//     .then((res) => {
+//         console.log(res);
+//         const found = (document.getElementById('found').innerHTML =
+//             'Found: ' + res.found);
+//         const number = (document.getElementById('number').innerHTML =
+//             'number ' + res.number);
+//         const text = (document.getElementById('text').innerHTML = res.text);
+//         const type = (document.getElementById('type').innerHTML =
+//             'number: ' + res.type);
+//     });
+// found.innerHTML = '';
+// number.innerHTML = '';
+// text.innerHTML = '';
+// type.innerHTML = '';
+// });
+
+// const API_URL = 'http://nu
